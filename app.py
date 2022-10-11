9bf026507ed5cc113c02374a507cbc890adf4f7f
M-Hawk <12922@coderacademy.edu.au>
2022-10-11 19:08:29 +1000

    update

--- app.py
+++ app.py
@@ -1,15 +1,56 @@
+from datetime import datetime
 from flask import Flask
+import json
+from random import randint
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def homepage():
+    return """<p>Hi, welcome to my API! Here are the endpoints that are available:</p>
+    <ul>
+    <li>Current Time: /time</li>
+    <li>Educator Info: /educators</li>
+    </ul>"""
 
-@app.route("/goodbye/")
-def goodbye_world():
-    return "<p>Goodbye, World!</p>"
+@app.route("/time/")
+def current_time():
+    time_dict = {"current time": str(datetime.now().strftime('%H:%M'))}
+    return json.dumps(time_dict)
 
-@app.route("/coder/")
-def coder():
-    return "<p>This web app was created in a class at Coder Academy.</p>"
+@app.route("/educators/")
+def educators():
+    educator_dict = {
+        "educators": [
+            {
+                "Name": "Oliver",
+                "Specialty": "Automated testing"
+            },
+            {
+                "Name": "Jairo",
+                "Specialty": "Discrete Mathematics"
+            },
+            {
+                "Name": "Amir",
+                "Specialty": "Web Development"
+            },
+            {
+                "Name": "Iryna",
+                "Specialty": "Database Engineering"
+            },
+            {
+                "Name": "George",
+                "Specialty": "Internet Security"
+            },
+        ]
+    }
+    return json.dumps(educator_dict)
+
+@app.route("/coinflip/")
+def coinflip():
+    result = randint(0, 1)
+    if result == 0:
+        result = {"result": "tails"}
+    elif result == 1:
+        result = {"result": "heads"}
+    return json.dumps(result)
